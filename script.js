78fc4751caffe0704e0a00beb7123e5f2eacf18b
Naveen Sharma <ns9660658523@gmail.com>
2025-06-29 09:22:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you! Your message has been sent.');
+  form.reset();
+});
